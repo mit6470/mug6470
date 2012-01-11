eb0c18dbbd1209acb2a2ebbed2c72c7901105845
Ying Yin <clevershadow@gmail.com>
2012-01-11 10:56:40 -0500

    Created a new rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mug6470::Application.config.secret_token = 'ccf104651d941aa0aa9a3e4da7ee650c8e41928b223312ddfe04aa06c9ad44a3c11aec16ea247bb9e0b1209be8402930df884692ff7c44dd4299149cae19bf95'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Mug6470::Application.config.session_store :cookie_store, key: '_mug6470_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Mug6470::Application.config.session_store :active_record_store
