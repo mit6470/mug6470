cbf91f2d2b9ebcf9d60c8627ef8a994e81388c24
Ying Yin <clevershadow@gmail.com>
2012-01-18 21:13:07 -0500

    WIP: working on parsing output.

--- app/models/trial.rb
+++ app/models/trial.rb
@@ -3,11 +3,17 @@ require 'open3'
 # A trial is a particular configuration of input data, classifier, and training
 # and testing modes.
 class Trial < ActiveRecord::Base
+  serialize output, Hash
+  
   # The classifier the user chose for this trial.
   belongs_to :classifier
   
   # The input data the user chose for this trial.
   belongs_to :datum
+  
+  validates :output, :length => 0..32.kilobytes, :allow_nil => true
+  
+  validates :name, :length => 0..32, 
  
   # Executes the trial and returns the result and error.
   #
@@ -26,8 +32,26 @@ class Trial < ActiveRecord::Base
       data_file = File.join ConfigVar[:data_dir], datum.file_name
       command = "java -cp #{classpath} #{classifier} -t #{data_file} -i"
       stdin, stdout, stderr = Open3.popen3 command
+      
       output = {}
-      output[:result] = stdout.readlines
+      
+      result = stdout.read   
+      rest, stratified = result && 
+                         result.split("=== Stratified cross-validation ===\n")
+      if stratified
+        rest, raw_matrix = stratified.split("=== Confusion Matrix ===\n")
+        if raw_matrix
+          confusion_matrix = []
+          raw_matrix = raw_matrix.split("\n").reject(&:blank?) 
+          confusion_matrix << raw_matrix[0].split('<--')[0].split(' ')
+          raw_matrix[1..-1].
+              each { |l| confusion_matrix << 
+                         l.split(/\s|\||\=/).reject(&:blank?) }
+          p confusion_matrix
+          output[:result] = {}
+          output[:result][:confusion_matrix] = confusion_matrix
+        end
+      end
       output[:error] = stderr.readlines
       output
     end 
--- db/migrate/20120115015524_create_trials.rb
+++ db/migrate/20120115015524_create_trials.rb
@@ -1,9 +1,11 @@
 class CreateTrials < ActiveRecord::Migration
   def change
     create_table :trials do |t|
+      t.string :name, :null => false, :limit => 32
       t.references :classifier, :null => true
       t.references :datum, :null => true
-
+      t.text :output, :null => true, :limit => 32.kilobytes
+      
       t.timestamps
     end
   end
