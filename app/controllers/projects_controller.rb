dd11bc3be19f4357347a83a5d813ba965814450a
Ying Yin <clevershadow@gmail.com>
2012-01-30 15:35:50 -0500

    Added some styles to project index.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -96,7 +96,7 @@ class ProjectsController < ApplicationController
     @project.destroy
 
     respond_to do |format|
-      format.html { redirect_to projects_url }
+      format.html { redirect_to :back }
       format.json { head :no_content }
     end
   end
--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,7 @@
 module ProjectsHelper
+  def close_tag
+    content_tag :span, 'Delete', :class => "ui-icon ui-icon-close", 
+                                 :title => "Delete"
+                       
+  end
 end
